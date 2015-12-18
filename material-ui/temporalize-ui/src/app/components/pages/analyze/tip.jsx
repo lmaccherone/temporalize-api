ad114d65bd7f197cd219396a918581bddaa81a0e
Larry Maccherone <Larry@Maccherone.com>
2015-12-18 12:35:06 -0500

    Starting over on TiP chart without using old code

--- material-ui/temporalize-ui/src/app/components/pages/analyze/tip.jsx
+++ material-ui/temporalize-ui/src/app/components/pages/analyze/tip.jsx
@@ -3,6 +3,7 @@ import Highcharts from 'highcharts-release/highcharts.src.js';
 import ReactHighcharts from 'react-highcharts/bundle/highcharts';
 import 'highcharts-exporting';
 import _ from 'lodash';
+import diff from "rfc6902-json-diff";
 import superagent from 'superagent/lib/client'
 
 import {ClearFix, Mixins, Paper} from 'material-ui';
@@ -15,44 +16,33 @@ import CodeBlock from '../../code-example/code-block';
 
 import tipCalculator from './tipCalculator.coffee';
 
+function deepEqual(obj1, obj2) {
+  var whereResult = _.where([obj1], obj2);
+  return _.where([obj1], obj2).length == 1;
+}
+
 const TiPChart = React.createClass({
 
   getInitialState: function() {
-    return {config: {}};
+    var cachedConfig = JSON.parse(localStorage.getItem('tip'));  // TODO: Update this to be a hash of 'tip' + this.state.userConfig
+    if (! cachedConfig) {
+      cachedConfig = {};
+    };
+    return {
+      config: cachedConfig
+    };
   },
 
   componentDidMount: function() {
-    var userConfig = {
-      subTitle: 'Stories In-Progress to Accepted',
-      debug: true,
-      trace: true,
-      daysToShow: 120,
-      // asOf: "2012-10-15",  // Optional. Only supply if want a specific time frame. Do not send in new Date().toISOString().
+    var cachedConfig;
 
-      scopeField: "_ProjectHierarchy",  // Supports Iteration, Release, Tags, Project, _ProjectHierarchy, _ItemHierarchy
-      scopeValue: 'scope',
-
-      statePredicate: {ScheduleState:{$lt:"Accepted", $gte:"In-Progress"}},
-      currentStatePredicate: {ScheduleState:{$gte:"Accepted"}},
-      type: 'HierarchicalRequirement',
-      leafOnly: true,
-      showTheseFieldsInToolTip: [ // Will automatically show ObjectID and Work Days In State
-        'Name',
-        {field: 'PlanEstimate', as: "Plan Estimate"}
-      ],
-      radiusField: {field: 'PlanEstimate', f: function(value){
-        if (isNaN(value)) {
-          return 5
-        } else {
-          return Math.pow(value, 0.6) + 5
-        }
-      }},
-      workDayStartOn: {hour: 9},
-      workDayEndBefore: {hour: 17},
-      // deriveFieldsOnSnapshotsConfig:
-      // holidays: (unless we pull them from some data model in Rally)
-      // workDays: (if you want to override the default pulling from WorkspaceConfiguration)
-    };
+    if (this.isMounted()) {
+      cachedConfig = JSON.parse(localStorage.getItem('tip'));  // TODO: Use the hash
+      if (! cachedConfig) cachedConfig = {};
+      if (! deepEqual(cachedConfig, this.state.config)) {
+        this.setState({config: cachedConfig});
+      };
+    }
 
     let lumenizeCalculatorConfig = {
       "config": {
@@ -78,156 +68,11 @@ const TiPChart = React.createClass({
 
 
 
-        var calculatorResults = tipCalculator({userConfig, lumenizeCalculatorConfig}, response.body);
+        var calculatorResults = tipCalculator({userConfig: this.state.userConfig, lumenizeCalculatorConfig}, response.body);
         var chartMax = 100;  // TODO: FIX THIS
 
+
         var scatterChartConfig = {
-          config: {
-            chart: {
-              renderTo: 'scatter-container',
-              defaultSeriesType: 'scatter',
-              zoomType: 'x',
-              marginTop: 80
-            },
-            legend: {
-              enabled: true,
-              floating: true,
-              align: 'center',
-              verticalAlign: 'top',
-              y: 37
-            },
-            credits: {
-              enabled: false
-            },
-            title: {
-              text: 'Time In Process (TiP)'
-            },
-            subtitle: {
-              text: userConfig.subTitle
-            },
-            xAxis: {
-              startOnTick: false,
-              tickmarkPlacement: 'on',
-              title: {
-                enabled: false
-              },
-              type: 'datetime',
-              min: calculatorResults.startMilliseconds,
-              max: calculatorResults.asOfMilliseconds
-            },
-            yAxis: [
-              {
-                title: {
-                  text: 'Time In Process (Work Days)'
-                },
-                opposite: false,
-                endOnTick: false,
-                //tickInterval: bucketSize,
-                //labels: {
-                //  formatter: function() {
-                //    if (this.value !== 0) {
-                //      if (this.value == chartMax) {
-                //        if (clipped) {
-                //          return '' + valueMax + '*';
-                //        } else {
-                //          return chartMax;
-                //        }
-                //      } else {
-                //        return this.value;
-                //      }
-                //    }
-                //  }
-                //},
-                min: 0,
-                max: chartMax
-              },
-              {
-                title: {
-                  text: null
-                },
-                opposite: true,
-                // endOnTick: true,
-                tickInterval: 1,
-                //labels: {
-                //  formatter: function() {
-                //    if (this.value !== 0) {
-                //      return Highcharts.numberFormat(buckets[this.value - 1].percentile * 100, 1) + "%";
-                //    } else {
-                //      return "0.0%";
-                //    }
-                //  }
-                //},
-                min: 0,
-                //max: buckets.length
-              }
-
-            ],
-            //tooltip: {
-              //formatter: function() {
-              //  var point = this.point;
-              //  tooltip = 'ObjectID: ' + point.ObjectID + '<br />';  // !TODO: Upgrade to link to revisions page in Rally
-              //  tooltip += this.series.name + ': <b>' + Highcharts.numberFormat(point.days, 1) + '</b> work days';
-              //  var t, _i, _len, _ref, f, field, as;
-              //  _ref = userConfig.showTheseFieldsInToolTip;
-              //  for (_i = 0, _len = _ref.length; _i < _len; _i++) {
-              //    t = _ref[_i];
-              //    if (typeof t === 'string') {
-              //      field = t;
-              //      f = function(value) {
-              //        return value;
-              //      };
-              //      as = t;
-              //    } else {
-              //      field = t.field;
-              //      if (t.f != null) {
-              //        f = t.f;
-              //      } else {
-              //        f = function(value) {
-              //          return value;
-              //        };
-              //      }
-              //      if (t.as != null) {
-              //        as = t.as;
-              //      } else {
-              //        as = t.field;
-              //      }
-              //    }
-              //    tooltip += '<br />' + as + ': ' + f(point[field + "_lastValue"]);
-              //  }
-              //  return tooltip;
-              //}
-            //},
-            plotOptions: {
-              scatter: {
-                marker: {
-                  states: {
-                    hover: {
-                      enabled: false
-                    }
-                  }
-                }
-              },
-              series: {
-                //events: {
-                //  legendItemClick: function(event) {
-                //    if (this.index == 0) {
-                //      if (!this.visible) {
-                //        this.chart.xAxis[0].setExtremes(calculatorResults.startMilliseconds, calculatorResults.asOfMilliseconds, false);
-                //      } else {
-                //        this.chart.xAxis[0].setExtremes(calculatorResults.asOfMilliseconds - 24 * 60 * 60 * 1000, calculatorResults.asOfMilliseconds, false);
-                //      };
-                //      this.chart.redraw();
-                //    };
-                //    return true;
-                //  }
-                //}
-              }
-            },
-            series: calculatorResults.series
-          }
-        };
-
-        scatterChartConfig = {
           subtitle: {
             text: 'Subtitle'
           },
@@ -242,13 +87,13 @@ const TiPChart = React.createClass({
 
         };
 
-        console.log('got to just before setting state. config', scatterChartConfig);
-        this.setState({
-          config: scatterChartConfig
-        });
-
-        console.log('forcing update');
-        this.forceUpdate();
+        if (deepEqual({a:1}, {b:2})) console.log('is equal', deepEqual({a:1}, {b:2}));
+        if (! deepEqual(cachedConfig, scatterChartConfig)) {
+          localStorage.setItem('tip', JSON.stringify(scatterChartConfig));  // TODO: Use hash
+          this.setState({
+            config: scatterChartConfig
+          });
+        };
       }
     }.bind(this));
   },
@@ -293,6 +138,7 @@ const TiPPage = React.createClass({
   },
 
 
+  // TODO: Set userConfig from the UI filter settings
 
   render() {
 
@@ -300,7 +146,7 @@ const TiPPage = React.createClass({
 
     return (
       <Paper style = {{marginBottom: '22px'}}>
-        <TiPChart />
+        <TiPChart userConfig={this.state.userConfig}/>
       </Paper>
     );
   },
