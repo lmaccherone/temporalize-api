bc1cc3cf3ea29a485da0c2ceeedfbe52251c45ad
Larry Maccherone <Larry@Maccherone.com>
2015-12-18 15:31:00 -0500

    Basically working

--- material-ui/temporalize-ui/src/app/components/pages/analyze/tip.jsx
+++ material-ui/temporalize-ui/src/app/components/pages/analyze/tip.jsx
@@ -58,139 +58,67 @@ const TiPChart = React.createClass({
         "stateFilter": {"State": {"$in": ["In Progress", "Accepted"]}},
         "granularity": "hour",
         "tz": "America/Chicago",
-        "endBefore": "2015-12-14T03:36:12.662Z",
+        "endBefore": "2015-12-18T03:36:12.662Z",
         "uniqueIDField": "_EntityID",
         "trackLastValueForTheseFields": ["_ValidTo", "Points"]
       }
     };
 
-    //let chart = this.refs.chart.getChart();
-
     superagent.post("/time-in-state")
       .accept('json')
       .send(lumenizeCalculatorConfig)
       .end(function(err, response) {
-
       console.log('response: ', response.body);
+
       if (this.isMounted()) {
-
-
-
         var calculatorResults = tipCalculator({userConfig: this.state.userConfig, lumenizeCalculatorConfig}, response.body);
-        var chartMax = 100;  // TODO: FIX THIS
-
+        var series = calculatorResults.series;
+        console.log('series: ', series)
 
         var scatterChartConfig = {
 
           chart: {
             type: 'bubble',
-            plotBorderWidth: 1,
-            zoomType: 'xy'
+            zoomType: 'x'
           },
-
           legend: {
-            enabled: false
+            enabled: true
           },
-
           title: {
-            text: 'Sugar and fat intake per country'
+            text: 'Time in Process (TiP)'
           },
-
           subtitle: {
-            text: 'Source: <a href="http://www.euromonitor.com/">Euromonitor</a> and <a href="https://data.oecd.org/">OECD</a>'
+            text: ''
           },
-
           xAxis: {
-            gridLineWidth: 1,
-            title: {
-              text: 'Daily fat intake'
-            },
-            labels: {
-              format: '{value} gr'
-            },
-            plotLines: [{
-              color: 'black',
-              dashStyle: 'dot',
-              width: 2,
-              value: 65,
-              label: {
-                rotation: 0,
-                y: 15,
-                style: {
-                  fontStyle: 'italic'
-                },
-                text: 'Safe fat intake 65g/day'
-              },
-              zIndex: 3
-            }]
+            type: 'datetime'
           },
-
           yAxis: {
-            startOnTick: false,
-            endOnTick: false,
             title: {
-              text: 'Daily sugar intake'
-            },
-            labels: {
-              format: '{value} gr'
-            },
-            maxPadding: 0.2,
-            plotLines: [{
-              color: 'black',
-              dashStyle: 'dot',
-              width: 2,
-              value: 50,
-              label: {
-                align: 'right',
-                style: {
-                  fontStyle: 'italic'
-                },
-                text: 'Safe sugar intake 50g/day',
-                x: -10
-              },
-              zIndex: 3
-            }]
+              text: 'Weeks in process'
+            }
           },
-
           tooltip: {
             useHTML: true,
             headerFormat: '<table>',
-            pointFormat: '<tr><th colspan="2"><h3>{point.country}</h3></th></tr>' +
-            '<tr><th>Fat intake:</th><td>{point.x}g</td></tr>' +
-            '<tr><th>Sugar intake:</th><td>{point.y}g</td></tr>' +
-            '<tr><th>Obesity (adults):</th><td>{point.z}%</td></tr>',
+            pointFormat: '<tr><th colspan="2"><h3>{point.name}</h3></th></tr>' +
+            '<tr><th>Last in process:</th><td>{point.dateLabel}</td></tr>' +
+            '<tr><th>Weeks in process:</th><td>{point.y}</td></tr>' +
+            '<tr><th>Size:</th><td>{point.z}</td></tr>',
             footerFormat: '</table>',
             followPointer: true
           },
 
-          plotOptions: {
-            series: {
-              dataLabels: {
-                enabled: true,
-                format: '{point.name}'
-              }
-            }
-          },
+          //plotOptions: {
+          //  series: {
+          //    dataLabels: {
+          //      enabled: true,
+          //      format: '{point.name}'
+          //    }
+          //  }
+          //},
 
-          series: [{
-            data: [
-              { x: 95, y: 95, z: 13.8, name: 'BE', country: 'Belgium' },
-              { x: 86.5, y: 102.9, z: 14.7, name: 'DE', country: 'Germany' },
-              { x: 80.8, y: 91.5, z: 15.8, name: 'FI', country: 'Finland' },
-              { x: 80.4, y: 102.5, z: 12, name: 'NL', country: 'Netherlands' },
-              { x: 80.3, y: 86.1, z: 11.8, name: 'SE', country: 'Sweden' },
-              { x: 78.4, y: 70.1, z: 16.6, name: 'ES', country: 'Spain' },
-              { x: 74.2, y: 68.5, z: 14.5, name: 'FR', country: 'France' },
-              { x: 73.5, y: 83.1, z: 10, name: 'NO', country: 'Norway' },
-              { x: 71, y: 93.2, z: 24.7, name: 'UK', country: 'United Kingdom' },
-              { x: 69.2, y: 57.6, z: 10.4, name: 'IT', country: 'Italy' },
-              { x: 68.6, y: 20, z: 16, name: 'RU', country: 'Russia' },
-              { x: 65.5, y: 126.4, z: 35.3, name: 'US', country: 'United States' },
-              { x: 65.4, y: 50.8, z: 28.5, name: 'HU', country: 'Hungary' },
-              { x: 63.4, y: 51.8, z: 15.4, name: 'PT', country: 'Portugal' },
-              { x: 64, y: 82.9, z: 31.3, name: 'NZ', country: 'New Zealand' }
-            ]
-          }]
+          series: series
 
         };
 
--- material-ui/temporalize-ui/src/app/components/pages/analyze/tipCalculator.coffee
+++ material-ui/temporalize-ui/src/app/components/pages/analyze/tipCalculator.coffee
@@ -3,8 +3,25 @@
 
 module.exports = (config, calculatorResults) ->
 
+  noLongerInProcess = []
+  stillInProcess = []
+  asOfDateString = "2015-12-18"  # TODO: Fix this
+  asOfDate = new Date(asOfDateString)
+  for row in calculatorResults
+    if row._ValidTo_lastValue > asOfDateString
+      row.x = asOfDate
+      row.dateLabel = 'still in process as of ' + asOfDateString
+      stillInProcess.push(row)
+    else
+      row.x = new Date(row._ValidTo_lastValue)
+      noLongerInProcess.push(row)
+      row.dateLabel = row._ValidTo_lastValue
+    row.y = row.ticks / 8 / 5  # Assumes 5 work-day week and 8 hours per day
+    row.z = row.Points_lastValue  # Assumes 5 work-day week and 8 hours per day
 
-  series = calculatorResults
+  series = [
+    {name: 'No longer in process', data: noLongerInProcess}
+    {name: 'Still in process', data: stillInProcess}
+  ]
 
-  visualizationData = {series}
-  return visualizationData
+  return {series}
